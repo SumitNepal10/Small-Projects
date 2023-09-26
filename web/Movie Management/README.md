3e7676712e4bb5d11102fb5bb9bc587cb25943b4
SumitNepal10 <nepalsumit30@gmail.com>
2023-09-26 18:27:39 +0545

    movie management initial commit

---
+++ web/Movie Management/README.md
@@ -0,0 +1,40 @@
+Movie Website
+This repository contains a movie website built using JSON to host a local server. The website provides a platform for users to browse and discover movies, view details and ratings, and watch trailers. It is designed to offer a seamless and engaging movie-watching experience.
+
+
+Features
+Movie Database: The website utilizes a JSON file as a local database, containing information about various movies such as title, description, release year and genre.
+
+Dynamic Content: The website dynamically loads movie data from the JSON file, allowing for easy updates and additions to the movie collection.
+
+Movie Listings: Users can explore a list of available movies, organized by different categories or genres.
+
+Movie Details: Each movie listing provides additional details including a brief description, release year, genre, and ratings.
+
+Trailer Playback: Users can watch movie trailers directly on the website by clicking the corresponding trailer link.
+
+Responsive Design: The website is built with a responsive layout, ensuring optimal viewing experience across different devices and screen sizes.
+
+Technologies Used
+
+HTML
+
+CSS
+
+JavaScript
+
+JSON
+
+Getting Started
+
+Clone this repository to your local machine.
+Open the project in your preferred code editor.
+Start a local server or use a tool like Live Server to run the website locally.
+Access the website in your browser by navigating to the local server address.
+
+Contributions
+Contributions to this project are welcome! If you find any issues or have suggestions for improvements, please feel free to open an issue or submit a pull request. Let's work together to make this movie website even better!
+
+License
+This project is licensed under the MIT License. Feel free to modify and distribute it as per the terms of the license.
+
---
+++ web/Movie Management/website/script.js
@@ -0,0 +1,118 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const searchResultContainer = document.querySelector(".searchResults");
+
+    // function to handle error while fetching data
+    async function fetchData() {
+        searchResultContainer.innerHTML = "";
+
+        try {
+            const response = await fetch("http://localhost:3000/movies");
+            const data = await response.json();
+
+            // check if data is available
+            if (data && data.length > 0) {
+                data.forEach(movie => {
+                    var movieElement = createMovieElement(movie);
+                    searchResultContainer.appendChild(movieElement);
+                });
+            } else {
+                const movieNotFound = document.createElement("h1");
+                movieNotFound.innerHTML = "Movie Not Found";
+                searchResultContainer.appendChild(movieNotFound);
+            }
+        } catch (error) {
+            console.error("Error fetching movie data:", error);
+        }
+    }
+
+    // function to search a particular movie
+    async function searchMovie(movieName) {
+        searchResultContainer.innerHTML = "";
+
+        try {
+            const response = await fetch("http://localhost:3000/movies");
+            const data = await response.json();
+
+            if (data && data.length > 0) {
+                data.forEach(movie => {
+                    if (movieName === movie.title) {
+                        var movieElement = createMovieElement(movie);
+                        searchResultContainer.appendChild(movieElement);
+                    }
+                });
+            } else {
+                const movieNotFound = document.createElement("h1");
+                movieNotFound.innerHTML = "Movie not Found!!";
+                searchResultContainer.appendChild(movieNotFound);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // function to  display the movie details
+    function createMovieElement(data) {
+        const movieContainer = document.createElement("div");
+        movieContainer.classList.add("movieContainer");
+
+        // display the title
+        const titleElement = document.createElement("h2");
+        titleElement.classList.add("title");
+        titleElement.innerHTML = `Title: ${data.title}`;
+        movieContainer.appendChild(titleElement);
+
+        const genreElement = document.createElement("p");
+        genreElement.classList.add("genre");
+        genreElement.innerHTML = `Genre: ${data.genre}`;
+        movieContainer.appendChild(genreElement);
+
+        const directorElement = document.createElement("p");
+        directorElement.classList.add("director");
+        directorElement.innerHTML = `Director: ${data.director}`;
+        movieContainer.appendChild(directorElement);
+
+        const releaseYearElement = document.createElement("p");
+        releaseYearElement.classList.add("release-year");
+        releaseYearElement.innerHTML = `Release Year: ${data.releaseYear}`;
+        movieContainer.appendChild(releaseYearElement);
+
+        const deleteButtonElement = document.createElement("button");
+        deleteButtonElement.classList.add("delButton");
+        deleteButtonElement.innerHTML = "Delete Movie";
+        deleteButtonElement.addEventListener("click", () => {
+            deleteMovie(data.id, movieContainer);
+        });
+        movieContainer.appendChild(deleteButtonElement);
+
+        return movieContainer;
+    }
+
+    function deleteMovie(movieId, movieElement) {
+        fetch(`http://localhost:3000/movies/${movieId}`, {
+            method: "DELETE"
+        })
+        .then(() => {
+            movieElement.remove();
+        })
+        .catch(error => {
+            console.error("Error deleting movie:", error);
+        });
+    }
+
+    // Searching movie
+    const searchButton = document.getElementById("submitButton");
+    searchButton.addEventListener("click", function () {
+        var movieName = document.getElementById("movieName").value;
+        searchMovie(movieName);
+    });
+
+    // search movie after pressing enter
+    var movieNameInput = document.getElementById("movieName");
+    movieNameInput.addEventListener('keyup', function (event) {
+        if (event.keyCode === 13) {
+            var movieName = movieNameInput.value;
+            searchMovie(movieName);
+        }
+    });
+    fetchData();
+});
